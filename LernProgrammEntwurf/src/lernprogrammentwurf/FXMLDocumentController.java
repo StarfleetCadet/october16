1594a784c20eeee791b9a99ccbec1da114fb059c
hhensel <h.hensel@develolabs.com>
2016-10-19 15:17:52 +0200

    test merge

--- LernProgrammEntwurf/src/lernprogrammentwurf/FXMLDocumentController.java
+++ LernProgrammEntwurf/src/lernprogrammentwurf/FXMLDocumentController.java
@@ -5,9 +5,6 @@
  */
 package lernprogrammentwurf;
 
-import java.net.URL;
-import java.util.Arrays;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -17,49 +14,59 @@ import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
 import lernprogrammentwurf.model.Question;
 
+import java.net.URL;
+import java.util.Arrays;
+import java.util.ResourceBundle;
+
 /**
  *
  * @author aviva
  */
-public class FXMLDocumentController implements Initializable 
+public class FXMLDocumentController implements Initializable
 {
-    
+
     @FXML
     private Label label;
-    
+
     @FXML
     private ComboBox<String> categoriesComboBox;
-    
+
     @FXML
     private TextArea questionField;
-    
+
     @FXML
     private TextArea answerField;
-    
+
     @FXML
     private Slider levelSlider;
-    
+
     @FXML
-    private void handleBtnAddQuestionAction(ActionEvent event) 
+    private void handleBtnAddQuestionAction(ActionEvent event)
     {
         Question toAdd = new Question();
+
+
         toAdd.setCategory(categoriesComboBox.getValue());
         toAdd.setCorrectAnswer(answerField.getText());
+
         toAdd.setLevel((int) (Math.round(levelSlider.getValue())));
         toAdd.setQuestion(questionField.getText());
+
+
         toAdd.add();
+
         questionField.clear();
-        answerField.clear();  
-        
+        answerField.clear();
+
     }
-    
+
     @Override
-    public void initialize(URL url, ResourceBundle rb) 
+    public void initialize(URL url, ResourceBundle rb)
     {
         Arrays.sort(Question.categories);
-        categoriesComboBox.getItems().addAll(Question.categories);     
-        
-    }    
-         
-    
+        categoriesComboBox.getItems().addAll(Question.categories);
+
+    }
+
+
 }
